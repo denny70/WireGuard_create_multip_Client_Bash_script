1b7f5340ee54dd5e94cb60a2a736a435ad6bc131
denny <denny70@gmail.com>
2019-01-08 13:24:49 +0800

    Update create_WG_one_client.sh

--- create_WG_one_client.sh
+++ create_WG_one_client.sh
@@ -16,7 +16,7 @@ clientName=$2
 #clientIP=10.0.0.6
 clientIP=10.0.0.$1
 #====================
-serverIP=111.222.333.242:10654
+serverIP=111.222.333.242:11154
 serverPublicKey=UpvqUFPaIHtqhp2nPpDXZozN0EOwqB+2EJ26mDnB83I=
 #====== need to modify part when changing server IP
 
